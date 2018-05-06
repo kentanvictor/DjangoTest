1de6d1941cff236c7f496995273729eb47ba8535
kentanvictor <2030457926@qq.com>
2018-05-06 17:34:00 +0800

    详细内容展示(多静态页面跳转)

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from booktest import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.index)
+    url(r'^$',views.index),
+    url(r'^(\d+)$',views.show)
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,6 +1,19 @@
 from django.http import *
+from django.template import RequestContext,loader
 from django.shortcuts import render
 
 # Create your views here.
+from booktest.models import *
+
+
 def index(request):
-    return HttpResponse('HelloWorld')
+    booklist = BookInfo.objects.all()
+    context = {'list':booklist}
+    return render(request,'booktest/index.html',context)
+
+
+def show(request,id):
+    book = BookInfo.objects.get(pk=id)
+    herolist=book.heroinfo_set.all()
+    context1 = {'list':herolist}
+    return render(request,'booktest/show.html',context1)
